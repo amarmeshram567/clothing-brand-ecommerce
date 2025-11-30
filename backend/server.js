77ebe0d32a94d14d0b3bf84c480043dd8a36f5f0
amarmeshram567 <meshramamar567@gmail.com>
2025-12-01 01:36:48 +0530

    fixing backend

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: [
+        "http://localhost:5173",
+        "https://clothing-brand-ecommerce-uip1.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
+
+app.options("*", cors());
 
 
 connectDB();
