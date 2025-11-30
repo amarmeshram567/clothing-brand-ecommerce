e000bfd351fe54464c7515efecd2c2999c5f7574
amarmeshram567 <meshramamar567@gmail.com>
2025-12-01 01:20:36 +0530

    fixing error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+    baseURL: import.meta.env.VITE_BACKEND_URL || 'https://clothing-brand-ecommerce.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
